dce9995989e6a026ba20bd8a84acd545e2828711
Alex Hanna <tinnvec@gmail.com>
2017-05-17 17:12:20 -0500

    Add PheromoneNetwork

--- src/index.d.ts
+++ src/index.d.ts
@@ -8,6 +8,16 @@ declare const __REVISION__: string;
 
 // --- Interfaces ---
 
+interface PheromoneNetwork {
+  layers: { [type: string]: CostMatrix }
+
+  dissipate(): void
+  draw(type: string, color?: string): void
+  getLevel(type: string, x: number, y: number): number
+  increaseLevel(type: string, x: number, y: number, amount: number): void
+  serialize(): { layers: { [type: string]: number[] }, roomName: string }
+}
+
 interface Room {
   readonly pheromoneNetwork: PheromoneNetwork
 
