53cd29bd4019fde67a9a28fb416b18f3717f2a5f
Alex Hanna <tinnvec@gmail.com>
2017-05-22 09:57:48 -0500

    Update to match tslint settings

--- libs/screeps-webpack-sources.ts
+++ libs/screeps-webpack-sources.ts
@@ -1,9 +1,9 @@
-import * as path from "path";
-import * as webpack from "webpack";
+import * as path from 'path'
+import * as webpack from 'webpack'
 
 // disable tslint rule, because we don't have types for these files
 /* tslint:disable:no-var-requires */
-const ConcatSource = require("webpack-sources").ConcatSource;
+const ConcatSource = require('webpack-sources').ConcatSource
 
 // Tiny tiny helper plugin that prepends "module.exports = " to all `.map` assets
 export class ScreepsSourceMapToJson implements webpack.Plugin {
@@ -12,14 +12,14 @@ export class ScreepsSourceMapToJson implements webpack.Plugin {
   // }
 
   public apply(compiler: webpack.Compiler) {
-    compiler.plugin("emit", (compilation, cb) => {
+    compiler.plugin('emit', (compilation, cb) => {
       for (const filename in compilation.assets) {
         // matches any files ending in ".map" or ".map.js"
-        if (path.basename(filename, ".js").match(/\.map/)) {
-          compilation.assets[filename] = new ConcatSource("module.exports = ", compilation.assets[filename]);
+        if (path.basename(filename, '.js').match(/\.map/)) {
+          compilation.assets[filename] = new ConcatSource('module.exports = ', compilation.assets[filename])
         }
       }
-      cb();
-    });
+      cb()
+    })
   }
 }
