711c89bf54def364316d117f1fc523bb84a68969
Alex Hanna <tinnvec@gmail.com>
2017-06-02 19:38:30 -0500

    Add Basic kernel class for managing processes

---
+++ src/components/Kernel.ts
@@ -0,0 +1,93 @@
+import { InitProcess } from '../processes/Init'
+import { Process, ProcessStatus } from './Process'
+import { ProcessRegistry } from './ProcessRegistry'
+
+export class Kernel {
+  public static processTable: { [pid: string]: Process } = {}
+
+  public static addProcess<T extends Process>(process: T) {
+    this.processTable[process.pid] = process
+    process.setMemory(this.getProcessMemory(process.pid))
+    process.status = ProcessStatus.Alive
+  }
+
+  public static getNextPID() {
+    Memory.pidCounter = Memory.pidCounter || 0
+    while (this.getProcessByPID(Memory.pidCounter) !== undefined) {
+      if (Memory.pidCounter >= Number.MAX_SAFE_INTEGER) { Memory.pidCounter = 0 }
+      Memory.pidCounter++
+    }
+    return Memory.pidCounter
+  }
+
+  public static getProcessByPID(pid: number) {
+    return this.processTable[pid]
+  }
+
+  public static getProcessMemory(pid: number) {
+    Memory.processMemory = Memory.processMemory || {}
+    Memory.processMemory[pid] = Memory.processMemory[pid] || {}
+    return Memory.processMemory[pid]
+  }
+
+  public static killProcess(pid: number) {
+    if (pid === 0) { return } // Can't kill process 0
+    // Kill children
+    for (const otherPid in this.processTable) {
+      const process = this.processTable[otherPid]
+      if (process.parentPID === pid && process.status !== ProcessStatus.Dead) { this.killProcess(process.pid) }
+    }
+    // Kill process
+    this.processTable[pid].status = ProcessStatus.Dead
+    Memory.processMemory[pid] = undefined
+  }
+
+  public static load() {
+    this.loadProcessTable()
+    this.garbageCollection()
+    if (this.getProcessByPID(0) === undefined) { InitProcess.start() }
+  }
+
+  public static run() {
+    while (this.queue.length > 0) {
+      let process = this.queue.pop()
+      while (process !== undefined) {
+        if (this.getProcessByPID(process.parentPID) === undefined) {
+          this.killProcess(process.pid)
+        }
+        if (process.status === ProcessStatus.Alive) { process.run() }
+        process = this.queue.pop()
+      }
+    }
+  }
+
+  public static save() {
+    this.storeProcessTable()
+  }
+
+  private static queue: Process[] = []
+
+  private static garbageCollection() {
+    Memory.processMemory = _.pick(Memory.processMemory, (_: any, k: string) => this.processTable[k] !== undefined)
+  }
+
+  private static loadProcessTable() {
+    this.processTable = {}
+    this.queue = []
+    Memory.processTable = Memory.processTable || []
+    for (const [pid, parentPID, processName, priority] of Memory.processTable) {
+      const processClass = ProcessRegistry.fetch(processName)
+      if (processClass === undefined) { continue }
+      const memory = this.getProcessMemory(pid)
+      const process = new processClass(parentPID, pid, priority)
+      process.setMemory(memory)
+      this.processTable[pid] = process
+      this.queue.push(process)
+    }
+  }
+
+  private static storeProcessTable() {
+    const liveProcs = _.filter(this.processTable, (p) => p.status !== ProcessStatus.Dead)
+    Memory.processTable = _.map(liveProcs, (p) => [p.pid, p.parentPID, p.constructor.name, p.priority])
+  }
+}
