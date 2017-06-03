9f3b0d731acc33fd3fc873dd26653a201f1dbe9d
Alex Hanna <tinnvec@gmail.com>
2017-06-03 11:29:07 -0500

    Add getDirectionPriorities method to CreepProcess

--- src/processes/Creep.ts
+++ src/processes/Creep.ts
@@ -15,5 +15,26 @@ export class CreepProcess extends Process {
     const creep = Game.creeps[this.memory.creepName]
     if (creep === undefined) { return Kernel.killProcess(this.pid) }
     if (creep.spawning) { return }
+
+  private getDirectionPriorities(lastDirection: number): number[] {
+    // 8 1 2
+    // 7 * 3
+    // 6 5 4
+    if (lastDirection === 0) {
+      return _.shuffle([1, 2, 3, 4, 5, 6, 7, 8])
+    }
+    const result: number[] = []
+    result.push(lastDirection)
+    for (let i = 1; i < 4; i++) {
+      let dl = lastDirection - i
+      if (dl < 1) { dl = dl + 8 }
+      let dr = lastDirection + i
+      if (dr > 8) { dr = dr - 8 }
+      result.push(..._.shuffle([dl, dr]))
+    }
+    let dRev = lastDirection + 4
+    if (dRev > 8) { dRev = dRev - 8 }
+    result.push(dRev)
+    return result
   }
 }
