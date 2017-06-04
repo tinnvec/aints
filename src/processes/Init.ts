f42386e83ad79f1e108507c23490353a171f5770
Alex Hanna <tinnvec@gmail.com>
2017-06-04 16:17:41 -0500

    Cleanup pidList parts in InitProcess, removing non-existing items

--- src/processes/Init.ts
+++ src/processes/Init.ts
@@ -23,18 +23,33 @@ export class InitProcess extends Process {
     }
 
     this.memory.pidList.spawns = this.memory.pidList.spawns || {}
+
+    for (const spawnName in this.memory.pidList.spawns) {
+      if (Game.spawns[spawnName] === undefined) { this.memory.pidList.spawns[spawnName] = undefined }
+    }
+
     for (const spawnName in Game.spawns) {
       const pid = this.memory.pidList.spawns[spawnName]
       if (pid === undefined) { this.memory.pidList.spawns[spawnName] = SpawnProcess.start(spawnName) }
     }
 
     this.memory.pidList.creeps = this.memory.pidList.creeps || {}
+
+    for (const creepName in this.memory.pidList.creeps) {
+      if (Game.creeps[creepName] === undefined) { this.memory.pidList.creeps[creepName] = undefined }
+    }
+
     for (const creepName in Game.creeps) {
       const pid = this.memory.pidList.creeps[creepName]
       if (pid === undefined) { this.memory.pidList.creeps[creepName] = CreepProcess.start(creepName) }
     }
 
     this.memory.pidList.rooms = this.memory.pidList.rooms || {}
+
+    for (const roomName in this.memory.pidList.rooms) {
+      if (Game.rooms[roomName] === undefined) { this.memory.pidList.rooms[roomName] = undefined }
+    }
+
     for (const roomName in Game.rooms) {
       const pid = this.memory.pidList.rooms[roomName]
       if (pid === undefined) { this.memory.pidList.rooms[roomName] = RoomProcess.start(roomName) }
