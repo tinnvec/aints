47c8ded1b2b6fd0da4b2a20a59dd1ec264ad9c91
Alex Hanna <tinnvec@gmail.com>
2017-06-07 07:50:29 -0500

    Update default config

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -5,9 +5,9 @@ import { LogLevels } from '../lib/logger/logLevels'
 export const ENABLE_ROOM_VISUALS: boolean = true
 
 export const PHEROMONE_MAX_TILE_AMOUNT: number = 255
-export const PHEROMONE_DECAY_TIME: number = 9
+export const PHEROMONE_DECAY_TIME: number = 25
 
-export const CREEP_MAX_SEARCH_STEPS: number = Math.floor(PHEROMONE_MAX_TILE_AMOUNT / 2)
+export const CREEP_MAX_SEARCH_STEPS: number = 100
 export const CREEP_MAX_STUCK_TICKS = 10
 export const CREEP_SEARCH_IGNORE_CREEPS_CHANCE = 0.25
 
